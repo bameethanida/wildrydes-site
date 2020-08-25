51e3f7d42546948de390b21944cdd8eaf9719acb
bameethanida <bameejrn@outlook.com>
2020-08-25 13:47:30 +0700

    Updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Un6dMQjoL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4e8vke1bs6o9clrv97c2jd83d6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
